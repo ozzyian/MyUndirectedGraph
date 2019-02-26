b87e49bcfaff805d699c0b7a8e0de3ad338e79be
Ozzyian <ogustafsson1@gmail.com>
2019-02-26 11:40:31 +0100

    java proj

---
+++ src/alda/graph/UndirectedGraphTest.java
@@ -0,0 +1,14 @@
+package alda.graph;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UndirectedGraphTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
