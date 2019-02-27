ea8e32887385b54fe68e2c977aedbfe031a152cc
Ozzyian <ogustafsson1@gmail.com>
2019-02-27 12:47:22 +0100

    node + edge class

--- src/alda/graph/MyUndirectedGraph.java
+++ src/alda/graph/MyUndirectedGraph.java
@@ -7,8 +7,32 @@ import java.util.List;
 
 public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 	
-	HashMap<T, List<GraphEdge<T>>> nodes = new HashMap<T, List<GraphEdge<T>>>();
+	HashMap<T, GraphNode<T>> nodes = new HashMap<T, GraphNode<T>>();
 	
+	class GraphNode<T>{
+		boolean visited;
+		T value;
+		List<GraphEdge<T>> adj;
+		public GraphNode(T value) {
+			this.value=value;
+			adj = new ArrayList<GraphEdge<T>>();
+		}
+		public GraphEdge<T> getExistingEdge(T from, T to){
+			for(GraphEdge<T> e : adj) {
+				if(e.arrivalNode.equals(to)) {
+					return e;
+				}
+			}
+			return null;
+		}
+		public int numberOfEdges() {
+			return adj.size();
+		}
+		
+		public void addEdge(GraphEdge<T> e) {
+			adj.add(e);
+		}
+	}
 	class GraphEdge<T>{
 		int weight;
 		T arrivalNode;
@@ -16,7 +40,6 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 			this.weight = weight;
 			arrivalNode = node;
 		}
-
 	}
 
 	@Override
@@ -28,8 +51,8 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 	@Override
 	public int getNumberOfEdges() {
 		int totalEdges = 0;
-		for(T node: nodes.keySet()) {
-			totalEdges+= nodes.get(node).size();
+		for(GraphNode<T> node: nodes.values()) {
+			totalEdges+= node.numberOfEdges();
 		}
 		return totalEdges/2;
 	}
@@ -39,21 +62,13 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 		if(nodes.containsKey(newNode))
 			return false;
 		else {
-			nodes.put(newNode, new ArrayList<GraphEdge<T>>());
+			nodes.put(newNode, new GraphNode<T>(newNode));
 			return true;
 		}
 			
 			
 	}
-	private GraphEdge<T> getExistingEdge(T from, T to){
-		List<GraphEdge<T>> fromEdges = nodes.get(from);
-		for(GraphEdge<T> e : fromEdges) {
-			if(e.arrivalNode.equals(to)) {
-				return e;
-			}
-		}
-		return null;
-	}
+
 
 	@Override
 	public boolean connect(T node1, T node2, int cost) {
@@ -63,14 +78,18 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 		GraphEdge<T> from = new GraphEdge<T>(cost, node2);
 		GraphEdge<T> to = new GraphEdge<T>(cost, node1);
 		
-		GraphEdge<T> existingEdge = getExistingEdge(node1, node2);
-		if(existingEdge!=null) {
-			existingEdge.weight = cost;
-			existingEdge = getExistingEdge(node2, node1);
-			existingEdge.weight = cost;
+		
+		
+		GraphEdge<T> existingEdge2 = nodes.get(node1).getExistingEdge(node1, node2);
+		//GraphEdge<T> existingEdge = getExistingEdge(node1, node2);
+		if(existingEdge2!=null) {
+			existingEdge2.weight = cost;
+			existingEdge2 = nodes.get(node2).getExistingEdge(node2, node1);
+			//existingEdge = getExistingEdge(node2, node1);
+			existingEdge2.weight = cost;
 		}else {
-			nodes.get(node1).add(from);
-			nodes.get(node2).add(to);
+			nodes.get(node1).addEdge(from);
+			nodes.get(node2).addEdge(to);
 		}
 		
 		return true;
@@ -83,7 +102,7 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 	public boolean isConnected(T node1, T node2) {
 		if(!nodes.containsKey(node1) || !nodes.containsKey(node2))
 			return false;
-		GraphEdge<T> existingEdge = getExistingEdge(node1, node2);
+		GraphEdge<T> existingEdge = nodes.get(node1).getExistingEdge(node1, node2);
 		if(existingEdge!=null) {
 			return true;
 		}
@@ -96,18 +115,21 @@ public class MyUndirectedGraph<T> implements UndirectedGraph<T> {
 		if(!nodes.containsKey(node1) || !nodes.containsKey(node2))
 			return -1;
 		
-		GraphEdge<T> existingEdge = getExistingEdge(node1, node2);
+		GraphEdge<T> existingEdge = nodes.get(node1).getExistingEdge(node1, node2);
 		if(existingEdge!=null) {
 			return existingEdge.weight;
 		}
 		return -1;
 
 	}
+	
+	
 
 	@Override
 	public List<T> depthFirstSearch(T start, T end) {
-		// TODO Auto-generated method stub
-		return null;
+		LinkedList<T> nodesToTravserse = new LinkedList<T>();
+		
+	return null;
 	}
 
 	@Override
